3543458bf58ce7a5b919dc33c913ab75c5df6359
Kazz2433 <kelvinquida24@gmail.com>
2023-05-19 10:16:49 -0400

    making requests api in transactions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import Summary from "../../components/Summary";
 import SearchForm from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
+
+interface Transactions {
+  id: number
+  description: string
+  type: 'income'|'outcome'
+  price: number
+  category: string
+  createdAt: string
+}
+
 export default function Transactions() {
+
+  const [transactions,setTransactions] = useState<Transactions[]>([])
+
+  async function loadTransactions(){
+    const response = await fetch("http://localhost:3333/transactions")
+    const data = await response.json()
+
+    setTransactions(data)
+  }
+
+  useEffect(()=>{
+    loadTransactions()
+  },[])
+
   return (
     <div>
         <Header />
@@ -13,26 +38,19 @@ export default function Transactions() {
         <SearchForm />
           <TransactionsTable>
             <tbody>
-              <tr>
-                <td width="50%">Desenvolvimento de Site</td>
+              {transactions.map(transaction => {
+                return(
+                <tr key={transaction.id}>
+                <td width="50%">{transaction.description}</td>
                 <td>
-                  <PriceHighlight variant="income">
-                    R$ 12.000,00
+                  <PriceHighlight variant={transaction.type}>
+                    {transaction.price}
                   </PriceHighlight>
                 </td>
-                <td>Vendo</td>
-                <td>13/04/2022</td>
-              </tr>
-              <tr>
-                <td width="50%">Hamburguer</td>
-                <td>
-                  <PriceHighlight variant="outcome">
-                    R$ 59,00
-                  </PriceHighlight>
-                </td>
-                <td>Alimentação</td>
-                <td>10/04/2022</td>
-              </tr>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.createdAt}</td>
+                </tr>
+              )})}
             </tbody>
           </TransactionsTable>
         </TransactionsContainer>
